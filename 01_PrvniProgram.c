09dc875e066cadc2bb95bd4e97f00030109896dd
zimmerhakl <tomas.zimmerhakl@gmail.com>
2024-09-12 13:14:05 +0200

    12.9. Promenna, ukazatel

--- 01_PrvniProgram.c
+++ 01_PrvniProgram.c
@@ -1,6 +1,18 @@
 #include "stdio.h"
 
 int main(void){
-	printf("Povedlo se.");
+	
+	int rychlost; // deklarace
+	int * adr_rychlost;
+
+	adr_rychlost = &rychlost;
+	rychlost = 25;
+	rychlost = rychlost * 2;
+
+	printf("Rychlost: %dkm/h\n", rychlost);
+	printf("Adresa rychlost: %p\n", &adr_rychlost);
+	printf("Rychlost z adresy: %d\n", *adr_rychlost);
+	
+
 	return 0;
 }
